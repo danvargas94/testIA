dfbc403884563913a5c31147857b11d7af53cc4b
Daniela Vargas <danvargas94@hotmail.com>
2017-02-13 22:34:31 -0600

    instructions

--- test.c
+++ test.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/********************************************************************************
+	For this assignment, you have to build a calculator that adds two numbers.
+	Input
+	The input consists of two lines, each containing one of the operands.
+	Output
+	Output the sum of the operands.
+*********************************************************************************/
+
 int main(){
 	int n1, n2;
 	scanf("%i", &n1);
